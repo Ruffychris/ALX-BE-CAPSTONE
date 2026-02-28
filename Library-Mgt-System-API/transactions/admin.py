a91f4c17cd83f754ba3723a7bc1be915e77a43d1
Ruffychris <christian.rufus23@gmail.com>
2026-02-28 10:16:06 +0000

    Register models in Django admin and configure admin interface

--- Library-Mgt-System-API/transactions/admin.py
+++ Library-Mgt-System-API/transactions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'book', 'checkout_date', 'return_date', 'is_returned')
+    list_filter = ('is_returned',)
+    search_fields = ('user__username', 'book__title')
--- Library-Mgt-System-API/users/admin.py
+++ Library-Mgt-System-API/users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_of_membership', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('user__username',)
