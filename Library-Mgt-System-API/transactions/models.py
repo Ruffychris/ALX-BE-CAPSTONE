3a877dac6627ea769050ce4d13a9b6b0fdf870be
Ruffychris <christian.rufus23@gmail.com>
2026-02-28 10:07:08 +0000

    Add database models for books, users and transactions

--- Library-Mgt-System-API/transactions/models.py
+++ Library-Mgt-System-API/transactions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from books.models import Book
 
-# Create your models here.
+
+class Transaction(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    checkout_date = models.DateTimeField(auto_now_add=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    is_returned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.book.title}"
