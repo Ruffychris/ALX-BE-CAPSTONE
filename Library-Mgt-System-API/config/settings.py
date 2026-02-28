80ba6d572415b3a344fc3a6f135921c498accfba
Ruffychris <christian.rufus23@gmail.com>
2026-02-28 11:53:37 +0000

    remove pagination

--- Library-Mgt-System-API/config/settings.py
+++ Library-Mgt-System-API/config/settings.py
@@ -127,7 +127,5 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
-        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 5
     ],
 }
